7214f41ee7f338a7bf68654758a2d5af4f943ed8
Mark Betnel <itmeson@gmail.com>
2013-05-15 11:11:40 -0400

    Removed rsync link to bnmnetp account.  Don't want to accidentally try to rsync to their server

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ if [ $# -ne 1 ]; 	then
 	echo 'need directory to deploy, relative to application base'
 	exit 1
 fi
-rsync -avz $1/ bnmnetp@web318.webfaction.com:~/webapps/web2py/web2py/applications/courselib/$1
+rsync -avz $1/ user@host.com:~/webapps/web2py/web2py/applications/courselib/$1
